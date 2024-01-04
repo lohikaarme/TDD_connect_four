f9b9bb4552ad3c82c14cc970c5dc1bac7534a576
lohikaarme <stewart.watt@gmail.com>
2024-01-03 22:21:43 -0800

    Add #player_move unit tests and code

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :p1, :p2, :current_turn
+  attr_accessor :p1, :p2, :current_turn, :move
 
   def initialize
     @p1 = { player: 1, sym: '🟡' }
@@ -15,5 +15,22 @@ class Player
     else
       @current_turn = @p1
     end
+  end
+
+  def player_move
+    i = true
+    while i
+      puts "Player #{@current_turn[:player]}, it is your turn, place your piece (1-7):"
+      begin
+        @move = Kernel.gets.match(/[1-7]/)[0].to_i
+      rescue StandardError
+        puts "\n-------------------------------"
+        puts '| Erroneous input, try again! |'
+        puts '-------------------------------'
+      else
+        i = false
+      end
+    end
+    @move
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,34 +3,52 @@
 require_relative '../lib/player'
 
 describe Player do
-  subject(:players) { described_class.new }
-
   describe '#initialize' do
+    subject(:players_init) { described_class.new }
     context 'players are created' do
       it 'creates player one with 🟡 symbol' do
-        player1 = players.p1
+        player1 = players_init.p1
         expect(player1[:player]).to eq(1)
         expect(player1[:sym]).to eq('🟡')
       end
 
       it 'creates player two is with 🔵 symbol' do
-        player2 = players.p2
+        player2 = players_init.p2
         expect(player2[:player]).to eq(2)
         expect(player2[:sym]).to eq('🔵')
       end
 
       it 'sets the current player to p1' do
-        player = players.current_turn
-        expect(player).to eq(players.p1)
+        player = players_init.current_turn
+        expect(player).to eq(players_init.p1)
       end
     end
   end
 
   describe '#end_turn' do
+    subject(:players_end) { described_class.new }
     context 'a player\'s turn ends' do
       it 'updates the current turn' do
-        expect { players.end_turn }.to change { players.current_turn }.from(players.p1).to(players.p2)
-        expect { players.end_turn }.to change { players.current_turn }.from(players.p2).to(players.p1)
+        expect { players_end.end_turn }.to change { players_end.current_turn }.from(players_end.p1).to(players_end.p2)
+        expect { players_end.end_turn }.to change { players_end.current_turn }.from(players_end.p2).to(players_end.p1)
+      end
+    end
+  end
+
+  describe '#player_move' do
+    subject(:players) { described_class.new }
+    before { allow($stdout).to receive(:puts) }
+
+    context 'when user enters an input' do
+      it 'receives a legal value' do
+        expect(Kernel).to receive(:gets).and_return('1')
+        expect { players.player_move }.to change { players.move }.from(nil).to(1)
+      end
+
+      it 'raises an error on an illegal value' do
+        expect(Kernel).to receive(:gets).and_raise('StandardError').once
+        expect(Kernel).to receive(:gets).and_return('1')
+        expect { players.player_move }.to output(/Erroneous input, try again!/).to_stdout
       end
     end
   end
