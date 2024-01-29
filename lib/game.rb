47be3d934e6271f66964bd2ed88b17952387ece9
lohikaarme <stewart.watt@gmail.com>
2024-01-29 06:55:33 -0800

    Cop corrections

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,7 @@ require_relative 'player'
 class Game
   attr_accessor :game_board, :players, :current_turn, :move, :winner, :game
 
-  def initialize (game_board = Board.new)
+  def initialize(game_board = Board.new)
     @game_board = game_board
     @players = { p1: Player.new(1, '🟡'), p2: Player.new(2, '🔵') }
     @current_turn = @players[:p1]
@@ -16,20 +16,20 @@ class Game
 
   def game_loop
     @game_board.print_board
-    @game_board.update_board(@current_turn, self.player_input)
+    @game_board.update_board(@current_turn, player_input)
     @winner = @game_board.won?(@game_board)
-    self.turn_change
-    self.continue?
+    turn_change
+    continue?
   end
 
   def turn_change
-    @current_turn == @players[:p1] ? @current_turn = @players[:p2] : @current_turn = @players[:p1]
+    @current_turn = @current_turn == @players[:p1] ? @players[:p2] : @players[:p1]
   end
 
   def continue?
     i = true
     while i
-        puts "Continue?: Enter(Yes) | Escape(No)"
+      puts 'Continue?: Enter(Yes) | Escape(No)'
       begin
         answer = Kernel.gets.match(/\n|\e/).to_s
       rescue StandardError
@@ -59,6 +59,4 @@ class Game
     end
     @move
   end
-
-
 end
